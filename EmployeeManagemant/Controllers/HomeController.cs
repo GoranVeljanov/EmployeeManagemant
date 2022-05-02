f5499ce348798ce8ebb7c5310a2b1cbbaccf0766
Goran <95771607+GoranVeljanov@users.noreply.github.com>
2022-05-02 23:36:32 +0200

    Edit Feature added

--- EmployeeManagemant/Controllers/HomeController.cs
+++ EmployeeManagemant/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿using EmployeeManagemant.Models;
 using Microsoft.AspNetCore.Mvc;
 using EmployeeManagemant.ViewModels;
-
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System;
+using Microsoft.AspNetCore.Http;
 
 namespace EmployeeManagemant.Controllers
 {
@@ -9,10 +12,14 @@ namespace EmployeeManagemant.Controllers
     public class HomeController : Controller
     {
         private readonly IEmployeeRepository _employeeRepository;
+        [Obsolete]
+        private readonly IHostingEnvironment hostingEnvironment;
 
-        public HomeController(IEmployeeRepository employeeRepository)
+        [Obsolete]
+        public HomeController(IEmployeeRepository employeeRepository, IHostingEnvironment hostingEnvironment)
         {
             _employeeRepository = employeeRepository;
+            this.hostingEnvironment = hostingEnvironment;
         }
       
         public ViewResult Index()
@@ -35,14 +42,92 @@ namespace EmployeeManagemant.Controllers
         public ViewResult Create()
         {
             return View();
+        }  
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Employee employee = _employeeRepository.GetEmployee(id);
+            EmployeeEditViewModel editViewModel = new EmployeeEditViewModel
+            {
+                Id = employee.Id,
+                Name = employee.Name,
+                Email = employee.Email,
+                Department = employee.Department,
+                ExistingPhotoPath = employee.PhotoPath
+            };
+            return View(editViewModel);
         }
         [HttpPost]
-        public IActionResult create(Employee employee)
+        [Obsolete]
+        public IActionResult Edit(EmployeeEditViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = _employeeRepository.GetEmployee(model.Id);
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Department = model.Department;
+                if(model.Photos != null)
+                {
+                    if(model.ExistingPhotoPath != null)
+                    {
+                        string filePath = Path.Combine(hostingEnvironment.WebRootPath, "images", model.ExistingPhotoPath);
+                        System.IO.File.Delete(filePath);
+                    }
+                    employee.PhotoPath = ProcessUploadedFile(model);
+                }
+
+                _employeeRepository.Update(employee);
+                return RedirectToAction("index");
+            }
+            return View();
+        }
+
+        private string ProcessUploadedFile(EmployeeCreateViewModel model)
+        {
+            string uniqueFileName = null;
+            if (model.Photos != null && model.Photos.Count > 0)
+            {
+                foreach (IFormFile photo in model.Photos)
+                {
+                    string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                    uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                    photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                }
+            }
+
+            return uniqueFileName;
+        }
+
+        [HttpPost]
+        public IActionResult Create(EmployeeCreateViewModel model)
         {
             if(ModelState.IsValid)
             {
-                Employee newEmployee = _employeeRepository.Add(employee);
-                //return RedirectToAction("details", new { id = newEmployee.Id });
+                string uniqueFileName = ProcessUploadedFile(model);
+                if(model.Photos != null && model.Photos.Count > 0)
+                {
+                    foreach (IFormFile photo in model.Photos)
+                    {
+                        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
+                        uniqueFileName = Guid.NewGuid().ToString() + "_" + photo.FileName;
+                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                        photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                    }
+                   
+                  
+                }
+                Employee newEmployee = new Employee
+                {
+                    Name = model.Name,
+                    Email = model.Email,
+                    Department = model.Department,
+                    PhotoPath = uniqueFileName
+
+                };
+                _employeeRepository.Add(newEmployee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
             }
             return View();
         }
---
+++ EmployeeManagemant/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagemant.Models;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagemant.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        
+        [Required]
+        [MaxLength(10, ErrorMessage = "Name cannot exceed 10 characters")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        ErrorMessage = "Invalid email format")]
+        [Display(Name = "Office Email")]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public List <IFormFile> Photos { get; set; }
+    }
+}
